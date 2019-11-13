41ed92919edf82ac0be50cb5351c845f2cc6de20
tlyon1022 <54088092+tlyon1022@users.noreply.github.com>
2019-11-13 12:30:30 -0500

    Add files via upload

---
+++ Prorgam 2/Prorgam 2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Prorgam_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptLbl = new System.Windows.Forms.Label();
+            this.creditLbl = new System.Windows.Forms.Label();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.creditTxt = new System.Windows.Forms.TextBox();
+            this.dayOutputLbl = new System.Windows.Forms.Label();
+            this.timeOutputLbl = new System.Windows.Forms.Label();
+            this.regCalcBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // promptLbl
+            // 
+            this.promptLbl.AutoSize = true;
+            this.promptLbl.Location = new System.Drawing.Point(38, 34);
+            this.promptLbl.Name = "promptLbl";
+            this.promptLbl.Size = new System.Drawing.Size(139, 16);
+            this.promptLbl.TabIndex = 0;
+            this.promptLbl.Text = "Enter your Last Name:";
+            // 
+            // creditLbl
+            // 
+            this.creditLbl.AutoSize = true;
+            this.creditLbl.Location = new System.Drawing.Point(38, 67);
+            this.creditLbl.Name = "creditLbl";
+            this.creditLbl.Size = new System.Drawing.Size(119, 16);
+            this.creditLbl.TabIndex = 1;
+            this.creditLbl.Text = "Enter Credit Hours:";
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(201, 31);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(100, 22);
+            this.nameTxt.TabIndex = 2;
+            // 
+            // creditTxt
+            // 
+            this.creditTxt.Location = new System.Drawing.Point(201, 64);
+            this.creditTxt.Name = "creditTxt";
+            this.creditTxt.Size = new System.Drawing.Size(100, 22);
+            this.creditTxt.TabIndex = 3;
+            // 
+            // dayOutputLbl
+            // 
+            this.dayOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dayOutputLbl.Location = new System.Drawing.Point(166, 125);
+            this.dayOutputLbl.Name = "dayOutputLbl";
+            this.dayOutputLbl.Size = new System.Drawing.Size(135, 23);
+            this.dayOutputLbl.TabIndex = 4;
+            // 
+            // timeOutputLbl
+            // 
+            this.timeOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeOutputLbl.Location = new System.Drawing.Point(166, 166);
+            this.timeOutputLbl.Name = "timeOutputLbl";
+            this.timeOutputLbl.Size = new System.Drawing.Size(135, 23);
+            this.timeOutputLbl.TabIndex = 5;
+            // 
+            // regCalcBtn
+            // 
+            this.regCalcBtn.Location = new System.Drawing.Point(78, 228);
+            this.regCalcBtn.Name = "regCalcBtn";
+            this.regCalcBtn.Size = new System.Drawing.Size(179, 35);
+            this.regCalcBtn.TabIndex = 6;
+            this.regCalcBtn.Text = "When Do I Register?";
+            this.regCalcBtn.UseVisualStyleBackColor = true;
+            this.regCalcBtn.Click += new System.EventHandler(this.RegCalcBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.regCalcBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 314);
+            this.Controls.Add(this.regCalcBtn);
+            this.Controls.Add(this.timeOutputLbl);
+            this.Controls.Add(this.dayOutputLbl);
+            this.Controls.Add(this.creditTxt);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.creditLbl);
+            this.Controls.Add(this.promptLbl);
+            this.Name = "Form1";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLbl;
+        private System.Windows.Forms.Label creditLbl;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.TextBox creditTxt;
+        private System.Windows.Forms.Label dayOutputLbl;
+        private System.Windows.Forms.Label timeOutputLbl;
+        private System.Windows.Forms.Button regCalcBtn;
+    }
+}
+
---
+++ Prorgam 2/Prorgam 2/Prorgam 2/Form1.cs
@@ -0,0 +1,162 @@
+﻿/*
+ K9195
+ CIS 199-02
+ October 16th, 2019
+ Program 2
+ This program will take a user's information about their credit hours and last name and tell them their registration date and time based on UofLs Spring 2020 criteria
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prorgam_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //this is the button event the user will execute after entering in information
+        private void RegCalcBtn_Click(object sender, EventArgs e)
+        {
+            const string FIRST_TIME_SLOT = "8:30AM",//constant for the firse time slot 
+                SECOND_TIME_SLOT = "10:00AM",//constant for the second time slot 
+                THIRD_TIME_SLOT = "11:30AM",//constant for the third time slot 
+                FOURTH_TIME_SLOT = "2:00PM",//constant for the fourth time slot 
+                LAST_TIME_SLOT = "4:00PM";//constant for the fifth and final time slot 
+            const string FIRST_DAY = "November 4th",//constant for the first day of registration
+                SECOND_DAY = "November 5th",//constant for the second day of registration
+                THIRD_DAY = "November 6th",//constant for the third day of registration
+                FOURTH_DAY = "November 7th",//constant for the fourth day of registration
+                FIFTH_DAY = "November 8th",//constant for the fifth day of registration
+                SIXTH_DAY = "November 11th";//constant for the sixth and final day of registration
+            const int UPPER_CLASS = 60,//classification of student based on 60 credit hours
+                LOWER_CLASS = 30,//classification of student based on 90 credit hours
+                SENIOR = 90;//classification of upperclass student to senior based on 90 hours
+
+            char last;//the last name variable for the student user
+            float credit;//credit hours for the student user
+
+            if (nameTxt.Text.Length > 0)//will ensure there is actually an input
+            {
+                last = nameTxt.Text[0];//reads the first letter of the Text box
+                if (char.IsLetter(last))//verifies there is a letter then makes it uppercase
+                {
+                    last = char.ToUpper(last);
+
+                    if (float.TryParse(creditTxt.Text, out credit))//requires the credit text box to be a float type 
+
+
+
+
+                    {
+                        if (credit >= UPPER_CLASS)//tests for 60+ hours
+                        {
+                            if (credit >= SENIOR)//assigns time slots for all credit hours 90+
+                            {
+                                dayOutputLbl.Text = $"{FIRST_DAY}";
+                                if (last <= 'D')
+                                    timeOutputLbl.Text = $"{THIRD_TIME_SLOT}";
+                                else if (last <= 'I')
+                                    timeOutputLbl.Text = $"{FOURTH_TIME_SLOT}";
+                                else if (last <= 'O')
+                                    timeOutputLbl.Text = $"{LAST_TIME_SLOT}";
+                                else if (last <= 'S')
+                                    timeOutputLbl.Text = $"{FIRST_TIME_SLOT}";
+                                else timeOutputLbl.Text = $"{SECOND_TIME_SLOT}";
+                            }
+
+
+
+                            else//tests for 60-90 credit hours
+                            {
+                                dayOutputLbl.Text = $"{SECOND_DAY}";
+                                if (last <= 'D')
+                                    timeOutputLbl.Text = $"{THIRD_TIME_SLOT}";
+                                else if (last <= 'I')
+                                    timeOutputLbl.Text = $"{FOURTH_TIME_SLOT}";
+                                else if (last <= 'O')
+                                    timeOutputLbl.Text = $"{LAST_TIME_SLOT}";
+                                else if (last <= 'S')
+                                    timeOutputLbl.Text = $"{FIRST_TIME_SLOT}";
+                                else timeOutputLbl.Text = $"{SECOND_TIME_SLOT}";
+                            }
+                        }
+
+
+
+                        else//finds the all students above 30 hours but less than 60 for their intial day 
+                        {
+                            if ((credit >= LOWER_CLASS) && (last <= 'B' || last >= 'P'))
+                            {
+                                dayOutputLbl.Text = $"{THIRD_DAY}";
+
+                                if (last <= 'B')
+                                    timeOutputLbl.Text = $"{LAST_TIME_SLOT}";
+                                else if (last <= 'Q')
+                                    timeOutputLbl.Text = $"{FIRST_TIME_SLOT}";
+                                else if (last <= 'S')
+                                    timeOutputLbl.Text = $"{SECOND_TIME_SLOT}";
+                                else if (last <= 'V')
+                                    timeOutputLbl.Text = $"{THIRD_TIME_SLOT}";
+                                else timeOutputLbl.Text = $"{FOURTH_TIME_SLOT}";
+                            }
+                            else if ((credit >= LOWER_CLASS) && (last <= 'O' && last >= 'C'))
+                            {
+                                dayOutputLbl.Text = $"{FOURTH_DAY}";
+                                if (last <= 'D')
+                                    timeOutputLbl.Text = $"{FIRST_TIME_SLOT}";
+                                else if (last <= 'F')
+                                    timeOutputLbl.Text = $"{SECOND_TIME_SLOT}";
+                                else if (last <= 'I')
+                                    timeOutputLbl.Text = $"{THIRD_TIME_SLOT}";
+                                else if (last <= 'L')
+                                    timeOutputLbl.Text = $"{FOURTH_TIME_SLOT}";
+                                else timeOutputLbl.Text = $"{LAST_TIME_SLOT}";
+                            }
+                            else if ((credit < LOWER_CLASS) && (last <= 'B' || last >= 'P'))//based on all freshman split into 2 days
+                            {
+                                dayOutputLbl.Text = $"{FIFTH_DAY}";
+                                if (last <= 'B')
+                                    timeOutputLbl.Text = $"{LAST_TIME_SLOT}";
+                                else if (last <= 'Q')
+                                    timeOutputLbl.Text = $"{FIRST_TIME_SLOT}";
+                                else if (last <= 'S')
+                                    timeOutputLbl.Text = $"{SECOND_TIME_SLOT}";
+                                else if (last <= 'V')
+                                    timeOutputLbl.Text = $"{THIRD_TIME_SLOT}";
+                                else timeOutputLbl.Text = $"{FOURTH_TIME_SLOT}";
+                            }
+                            else
+                            {
+                                dayOutputLbl.Text = $"{SIXTH_DAY}";
+                                if (last <= 'D')
+                                    timeOutputLbl.Text = $"{FIRST_TIME_SLOT}";
+                                else if (last <= 'F')
+                                    timeOutputLbl.Text = $"{SECOND_TIME_SLOT}";
+                                else if (last <= 'I')
+                                    timeOutputLbl.Text = $"{THIRD_TIME_SLOT}";
+                                else if (last <= 'L')
+                                    timeOutputLbl.Text = $"{FOURTH_TIME_SLOT}";
+                                else timeOutputLbl.Text = $"{LAST_TIME_SLOT}";
+                            }
+                        }
+                    }
+                    else MessageBox.Show("Please enter a proper credit hour");
+                } 
+
+                else MessageBox.Show("Please enter a a valid last name");
+            }
+            else MessageBox.Show("Please enter a valid last name");
+        
+        }
+        
+    }
+}
